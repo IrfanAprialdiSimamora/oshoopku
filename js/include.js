bcb078465f1f097227d78b70f2e706b50e195bca
IrfanAprialdiSimamora <irfansimamora58186@gmail.com>
2025-10-22 06:50:01 +0700

    first commit

---
+++ js/include.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", () => {
+  document.querySelectorAll("[data-include]").forEach(async (el) => {
+    const file = el.getAttribute("data-include");
+    const response = await fetch(file);
+    if (response.ok) {
+      el.innerHTML = await response.text();
+    } else {
+      el.innerHTML = "<p>Gagal Memuat" + file + "</p>";
+    }
+  });
+});
